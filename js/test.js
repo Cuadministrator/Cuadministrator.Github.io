40ebf45af3fb37833c1985bb268056efabcce68d
cuadministrator <2469084112@qq.com>
2017-11-20 16:18:56 +0800

    update

--- js/test.js
+++ js/test.js
@@ -2,10 +2,10 @@ function btnclick (){
     if(window.__wxjs_environment === 'miniprogram'){
         console.log(window.__wxjs_environment === 'miniprogram')
         wx.miniProgram.navigateTo({
-            url:'/pages/index/index',
-            success: ()=>{console.log("reLaunch Success")},
-            fail: ()=>{console.log("reLaunch Fail")},
-            complete: ()=>{console.log("reLaunch Final")},
+            url:'/pages/inquiry/inquiry?inquiryKey=5564330985548330207&uuid=f7723935-21b2-4a67-84c1-57ec05122929&categoryId=1',
+            success: ()=>{console.log("navigateTo Success")},
+            fail: ()=>{console.log("navigateTo Fail")},
+            complete: ()=>{console.log("navigateTo Final")},
         })
     }
 }
