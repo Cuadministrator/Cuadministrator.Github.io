116cd8333e3b327270b94d8118cd55db9c3f3690
cuadministrator <2469084112@qq.com>
2017-11-20 16:08:58 +0800

    update

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 function btnclick (){
     if(window.__wxjs_environment === 'miniprogram'){
         console.log(window.__wxjs_environment === 'miniprogram')
-        wx.miniProgram.reLaunch({
+        wx.miniProgram.navigateTo({
             url:'/pages/index/index',
             success: ()=>{console.log("reLaunch Success")},
             fail: ()=>{console.log("reLaunch Fail")},
